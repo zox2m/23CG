229298d658e667dbb225faec9eac84dff657e2f9
dmswneunju <ejyhej1004@gmail.com>
2023-11-21 16:54:07 +0900

    편지 클릭하면 ui띄우기!!

---
+++ My project/Assets/script/Letter_hint.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace SojaExiles
+{
+public class Letter_hint : MonoBehaviour
+    {
+        public GameObject letterHintUI; // 새 힌트 UI
+        public GameObject player;
+        public Camera camera;
+        
+        public void Start(){
+            letterHintUI.SetActive(false);
+        }
+        
+        public void OnMouseDown() {
+            if (ObjectClicker.uiIsActived == false) {
+                Debug.Log("click Painting");
+                letterHintUI.SetActive(true); // 힌트 UI 활성화
+                player.GetComponent<PlayerMovement>().enabled = false;
+                camera.GetComponent<MouseLook>().enabled = false;
+                ObjectClicker.uiIsActived = true;
+            }
+        }
+        
+        public void CloseLetterHint() {
+            Debug.Log("close painting hint ui");
+            letterHintUI.SetActive(false); // 힌트 UI 비활성화
+            player.GetComponent<PlayerMovement>().enabled = true;
+            camera.GetComponent<MouseLook>().enabled = true;
+            ObjectClicker.uiIsActived = false;
+        }
+        
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                CloseLetterHint();
+            }
+        }
+    }        
+}
