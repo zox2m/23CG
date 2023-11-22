58e0f8f98fd3b26a1783185fa673911f8099f6d9
CHOI_SEUNG_YEON\steph <stephanie1631@naver.com>
2023-11-22 15:46:31 +0900

    편지읽을때 소리

--- My project/Assets/script/Letter_hint.cs
+++ My project/Assets/script/Letter_hint.cs
@@ -11,15 +11,22 @@ public class Letter_hint : MonoBehaviour
         public GameObject letterHintUI; // 새 힌트 UI
         public GameObject player;
         public Camera camera;
-        
+
+        // 오디오 소스 추가 
+        private AudioSource audioSource;
+        private AudioClip drawerOpenSound;
+
         public void Start(){
             letterHintUI.SetActive(false);
+            audioSource = this.gameObject.GetComponent<AudioSource>();
         }
         
         public void OnMouseDown() {
             if (ObjectClicker.uiIsActived == false) {
                 Debug.Log("click Painting");
                 letterHintUI.SetActive(true); // 힌트 UI 활성화
+
+                this.audioSource.Play();
                 player.GetComponent<PlayerMovement>().enabled = false;
                 camera.GetComponent<MouseLook>().enabled = false;
                 ObjectClicker.uiIsActived = true;
@@ -29,6 +36,8 @@ public class Letter_hint : MonoBehaviour
         public void CloseLetterHint() {
             Debug.Log("close painting hint ui");
             letterHintUI.SetActive(false); // 힌트 UI 비활성화
+
+            this.audioSource.Stop();
             player.GetComponent<PlayerMovement>().enabled = true;
             camera.GetComponent<MouseLook>().enabled = true;
             ObjectClicker.uiIsActived = false;
