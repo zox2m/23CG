a209d5c3b1fbe34958a87d2b533aef0f6f6f4a2f
zox2m <zox2mm@gmail.com>
2023-11-24 11:54:03 +0900

    3번방 키패드 수정 완료

--- My project/Assets/Keypad/Scripts/Keypad2.cs
+++ My project/Assets/Keypad/Scripts/Keypad2.cs
@@ -77,7 +77,7 @@ public class Keypad2 : MonoBehaviour
         if(int.TryParse(currentInput, out var currentKombo))
         {
             //bool granted = currentKombo == keypadCombo;
-            bool granted = currentKombo == 0125;
+            bool granted = currentKombo == 2222;
             if (!displayingResult)
             {
                 StartCoroutine(DisplayResultRoutine(granted));
@@ -130,7 +130,7 @@ public class Keypad2 : MonoBehaviour
         panelMesh.material.SetVector("_EmissionColor", screenGrantedColor * screenIntensity);
         audioSource.PlayOneShot(accessGrantedSfx);
         //문 열림 변수 수정해줌 
-        opencloseDoor.door1Open = true;
+        opencloseDoor2.door1Open = true;
     }
 
 }
