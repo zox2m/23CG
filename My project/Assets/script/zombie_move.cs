e41de3ca8a6743fa62005d0a8b9419cec0935e4e
zox2m <zox2mm@gmail.com>
2023-12-10 19:25:35 +0900

    좀비 기본적으로 복도 순찰, 플레이어 발견시 추적 - 범위 수정 필요

--- My project/Assets/script/zombie_move.cs
+++ My project/Assets/script/zombie_move.cs
@@ -4,52 +4,74 @@ using UnityEngine;
 
 public class zombie_move : MonoBehaviour
 {
+    // 추적을 위한 값들 
     Rigidbody rb;
     Transform target;
     [Header("추격 속도")]
-    [SerializeField] [ Range(1f,4f)] float moveSpeed =3f;
-    [Header("근접 거리")]
-    [SerializeField] [ Range(0f,3f)] float contactDistance =1f;
-    bool follow = false;
+    [SerializeField] [ Range(1f,4f)] float traceSpeed =1f;
+    
+    [Header("추적 거리")]
+    [SerializeField] [ Range(0f,3f)] float traceDistance =1f;
+
+    // 이 객체에서 사용할 웨이오핀트 참조 
+    [SerializeField] private Waypoints waypoints;
+
+    //기본 이동 속도
+    [Header("기본 속도")]
+    [SerializeField] private float moveSpeed = 1f;
+
+    //웨이포인트와의 거리 
+    [SerializeField] private float distanceThreshold = 0.1f;
+
+    // 현재 타겟이 되는 웨이포인트 
+    private Transform currentWaypoint;
 
     //start
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // 컴포넌트 연결 
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); // player의 트랜스폼 값 가져옴 
+
+        // 웨이포인트 1번으로 이동 
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        //다음 웨이포인트를 타겟으로 정하기 
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+
+        // 웨이포인트 타겟 지점 바라보기 
+        transform.LookAt(currentWaypoint);
     }
-    // Update is called once per frame
+    
     void Update()
     {
-        //transform.Translate(Vector3.forward * Time.deltaTime); 앞으로 움직임 
-        FollowTarget();
+        // 플레이어가 추적 범위 내에 있다면
+        if(Vector3.Distance(transform.position,target.position) < traceDistance){
+            FollowTarget();
+        }
+
+        //아니라면~ patrol 
+        else{
+            transform.LookAt(currentWaypoint);
+
+            transform.position = Vector3.MoveTowards(transform.position,currentWaypoint.position,moveSpeed * Time.deltaTime);
+
+            if(Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+            {
+                currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+                transform.LookAt(currentWaypoint);
+            }
+        }       
+
     }
 
     void FollowTarget()
     {
-        if(Vector3.Distance(transform.position,target.position) > contactDistance &&follow){
-            //이게 원본코드인데, y방향으로도 따라와서 문제이다. 플레이어 기준점이 공중에 있나봄 ;;
-            transform.position = Vector3.MoveTowards(transform.position, target.position,moveSpeed*Time.deltaTime);
-            
-            //타겟 방향보기 
-            // Vector3 dir = target.transform.position - this.transform.position;
+        // 플레이어 방향으로 움직이기 
+        transform.position = Vector3.MoveTowards(transform.position, target.position,traceSpeed*Time.deltaTime);
+        
+        //타겟 방향보기 
+        transform.LookAt(target);
 
-            
-			// this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * moveSpeed);
-            // lookat 함수 있네;;; 개킹받아 
-            transform.LookAt(target);
-        }
-        else   
-            rb.velocity = Vector3.zero;
-    }
-
-    private void OnTriggerEnter(Collider collision)
-    {
-        follow = true;
-    }
-
-    private void OnTriggerExid(Collider collision)
-    {
-        follow = false;
     }
 }
